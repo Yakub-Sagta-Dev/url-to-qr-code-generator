77a69995d006eccc45aad8caa396ee590ebc5ef0
Yakub Dev <yakub@sagta.com>
2023-09-04 17:00:20 +0800

    Function side is completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  link: string = 'www.sagta.com';
+  inputUrl: string = '';
+  inputName: string = '';
+  qrCodeDownloadLink: SafeUrl = '';
+
+  generateQR() {
+    this.link = this.inputUrl;
+  }
+
+  onChangeUrl(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+}
